33bba8174e0d1b9ec3d9f2f563cbeda93f9ef608
happilyeverafter95 <mgu@wealthsimple.com>
2020-10-18 22:00:56 -0400

    all code

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# tensorflow-serving-2.0
+# Tensorflow Serving 2.0
+
+Serving a sentiment analysis model (movie review sentiment) using Tensorflow 2.0 and Tensorflow serving.
+
+If you're using Tensorflow 1.X, please refer to my [toxic-comment-classifier](https://github.com/happilyeverafter95/toxic-comment-classifer) for a serving example with a text classifier. I would recommend migrating to the newer versions as Tensorflow 2.X introduces a lot of additional support for NLP models.
+
+## Usage Requirements
+
+Training script written and tested for in Python 3.8
+
+* Install all Python dependencies using `pip install -r requirements.txt`
+
+To train the model and output the SavedModel object, run `python -m model.train`. This will output the SavedModel files to `classifier/saved_models/1`.
+
+## Serve the Model
+
+For more information, refer to the official [tensorflow/serving repo](https://github.com/tensorflow/serving).
+
+### Start the Server
+
+Instructions paraphrased and adapted from the TensorFlow Serving repo.
+
+1. Install Docker
+2. Fetch the latest version of TensorFlow Serving Docker docker pull tensorflow/serving
+3. Specify the directory for export. In the root directory of this repo, run ModelPath="$(pwd)/classifier"
+   
+**To start the server:**
+
+docker run -t --rm -p 8501:8501 \
+    -v "$ModelPath/:/saved_models/sentiment_analysis" \
+    -e MODEL_NAME=sentiment_analysis \
+    tensorflow/serving
+
+### Sample Curl Command
+
+```
+curl -d '{"signature_name": "predict","inputs":{"input": "raw text goes here"}}' \
+  -X POST http://localhost:8501/v1/models/sentiment_analysis:predict
+```
+
+All preprocessing steps are applied to the input prior to prediction. You might also notice that the payload has an additional parameter alongside the actual prediction. This was defined as a part of the model signature and can be customized to provide more meaningful metadata.
---
+++ classifier/train.py
@@ -0,0 +1,79 @@
+import logging
+import re
+import string
+from typing import Tuple, Union, List, Dict
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+import tensorflow_datasets as tfds
+
+level = logging.INFO
+logging.basicConfig(level=level)
+logger = logging.getLogger(__name__)
+
+
+class TFModel(tf.Module):
+    def __init__(self, model: tf.keras.Model) -> None:
+        self.model = model
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=(1, ), dtype=tf.string)])
+    def prediction(self, review: str) -> Dict[str, Union[str, List[float]]]:
+        return {'prediction': self.model(review),
+                'description': 'prediction ranges from 0 (negative) to 1 (positive)'}
+
+
+class ModelTrainer:
+    def __init__(self) -> None:
+        self.tf_model_wrapper: TFModel
+
+        # Model Architecture parameters
+        self.embed_size = 128
+        self.max_features = 20000
+        self.epochs = 10
+        self.batch_size = 128
+        self.max_len = 500
+
+    def fetch_data(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        train_data, test_data = tfds.load(name='imdb_reviews', split=['train', 'test'],
+                                          batch_size=-1, as_supervised=True)
+
+        train_examples, train_labels = tfds.as_numpy(train_data)
+        logger.info(f'There are {train_examples.shape[0]} reviews in the training set')
+        test_examples, test_labels = tfds.as_numpy(test_data)
+        logger.info(f'There are {test_examples.shape[0]} reviews in the testing set')
+        return train_examples, train_labels, test_examples, test_labels
+
+    def custom_preprocessing(self, raw_text: str) -> tf.string:
+        lowercase = tf.strings.lower(raw_text)
+        stripped_html = tf.strings.regex_replace(lowercase, "<br />", " ")
+        return tf.strings.regex_replace(stripped_html, "[%s]" % re.escape(string.punctuation), '')
+
+    def init_vectorize_layer(self, text_dataset: np.ndarray) -> TextVectorization:
+        text_vectorizer = TextVectorization(max_tokens=self.max_features,
+                                            standardize=self.custom_preprocessing,
+                                            output_mode='int',
+                                            output_sequence_length=self.max_len)
+        text_vectorizer.adapt(text_dataset)
+        return text_vectorizer
+
+    def init_model(self, text_dataset: np.ndarray) -> tf.keras.Model:
+        vectorize_layer = self.init_vectorize_layer(text_dataset)
+        raw_input = tf.keras.Input(shape=(1,), dtype=tf.string)
+        x = vectorize_layer(raw_input)
+        x = tf.keras.layers.Embedding(self.max_features + 1, self.embed_size)(x)
+        x = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(100, return_sequences=True))(x)
+        x = tf.keras.layers.GlobalMaxPooling1D()(x)
+        predictions = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+        model = tf.keras.Model(raw_input, predictions)
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
+
+    def train(self) -> None:
+        train_examples, train_labels, _, _ = self.fetch_data()
+        model = self.init_model(train_examples)
+        model.fit(train_examples, train_labels, epochs=self.epochs, batch_size=self.batch_size)
+        self.tf_model_wrapper = TFModel(model)
+        tf.saved_model.save(self.tf_model_wrapper.model, 'classifier/saved_models/1',
+                            signatures={'serving_default': self.tf_model_wrapper.prediction})
+        logger.info('saving SavedModel to saved_models/1')
